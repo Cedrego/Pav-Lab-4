1b3d058ab2d71b688fd6fa2682af93d0ed1c6a54
Desckars <enzbrun8@gmail.com>
2024-06-18 16:46:42 -0300

    listo

---
+++ deCompletar.cpp
@@ -0,0 +1,12 @@
+#include "deCompletar.h"
+
+deCompletar::~deCompletar(){};
+
+string deCompletar::getfraseC(){
+    return this->fraseC;
+};
+/*
+ICollection* deCompletar::getfaltante(){
+    return faltante;
+};
+*/
